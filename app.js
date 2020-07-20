b9c649a7e24b9d75361d8bc2fcf44e18b703bf3c
AnanyAbellop <padronalbany@gmail.com>
2020-07-20 01:21:10 +0000

    Primera aproximación del domain generation. no agregue mas elementos en los array

---
+++ app.js
@@ -0,0 +1,19 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+for (let i=0;i<pronoun.length;i++){
+    for (let j=0;j<adj.length;j++){
+        for (let k=0;k<noun.length;k++){
+            
+            let a=Math.floor((Math.random()*pronoun.length));
+            let b=Math.floor((Math.random()*adj.length));
+            let c=Math.floor((Math.random()*noun.length));
+
+            var domain= pronoun[a]+adj[b]+noun[c]+".com";
+            console.log(a,b,c);
+            console.log(domain);
+
+        }
+    }
+}
